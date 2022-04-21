9a5aea19ab92ab3e79ea33dd46c06eec78ddeb73
Ubar <bellin83@gmail.com>
2022-04-22 05:25:01 +0900

    Add: TailwindCSS Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">it works</h1>
+    </div>
+  );
 }
 
 export default Home
