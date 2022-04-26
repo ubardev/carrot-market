73bc1c0ab91c36b5d65c796385dd407f953ee7b5
Ubar <bellin83@gmail.com>
2022-04-27 08:05:13 +0900

    :sparkles: 4.11 Dark Mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,28 +7,21 @@ const Home: NextPage = () => {
   return (
     <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
       {/* Select Item Start */}
-      <div className="bg-white flex flex-col justify-between p-6 rounded-2xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+      <div className="bg-white dark:bg-black flex flex-col justify-between p-6 rounded-2xl shadow-xl">
+        <span className="font-semibold dark:text-white text-3xl">Select Item</span>
         <ul>
           {[1, 2, 3, 4, 5].map((i) => (
-            <div key={i} className="flex justify-between my-2 odd:bg-blue-500 even:bg-yellow-500">
-              <span className="text-gray-500">Grey Chair</span>
-              <span className="font-semibold">$19</span>
+            <div key={i} className="flex justify-between my-2">
+              <span className="text-gray-500 dark:text-gray-100">Grey Chair</span>
+              <span className="font-semibold dark:text-white">$19</span>
             </div>
-          ))}
-        </ul>
-        <ul>
-          {['a', 'b', 'c', ''].map((c, i) => (
-            <li className="bg-red-500 py-2 empty:hidden" key={i}>
-              {c}
-            </li>
           ))}
         </ul>
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+        <div className="mt-5 bg-blue-500 dark:bg-black dark:border-white dark:border text-white p-3 text-center rounded-xl w-1/2 mx-auto dark:hover:bg-white hover:bg-teal-500 dark:hover:text-black active:bg-yellow-500 focus:bg-red-500">
           Checkout
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
+  darkMode: 'media',
   plugins: [],
 };
