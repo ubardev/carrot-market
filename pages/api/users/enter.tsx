7f4cb14b15abf54131a38c0ad8b994fce0c62e16
Ubar <bellin83@gmail.com>
2022-05-13 06:06:44 +0900

    :sparkles: 9.6 Token UI

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -34,22 +34,21 @@ async function handler(
   });
 
   if (phone) {
-    const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE!,
-      body: `Your login token is ${payload}.`,
-    });
-
-    console.log(message);
+    // // const message = await twilioClient.messages.create({
+    // //   messagingServiceSid: process.env.TWILIO_MSID,
+    // //   to: process.env.MY_PHONE!,
+    // //   body: `Your login token is ${payload}.`,
+    // // });
+    // console.log(message);
   } else if (email) {
-    const email = await mail.send({
-      from: "austin.hwang@tosslab.com",
-      to: "bellin83@gmail.com",
-      subject: "Your Carrot Market Verification Email",
-      text: `Your token is ${payload}`,
-      html: `<strong>Your token is ${payload}</strong>`,
-    });
-    console.log(email);
+    // const email = await mail.send({
+    //   from: "austin.hwang@tosslab.com",
+    //   to: "bellin83@gmail.com",
+    //   subject: "Your Carrot Market Verification Email",
+    //   text: `Your token is ${payload}`,
+    //   html: `<strong>Your token is ${payload}</strong>`,
+    // });
+    // console.log(email);
   }
 
   return res.json({
