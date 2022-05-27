c097b3c3c9a5162767ca750ce4b502dbf71496e0
Ubar <bellin83@gmail.com>
2022-05-28 06:43:35 +0900

    :sparkles: 15.4 Direct Upload URL

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -39,18 +39,30 @@ const EditProfile: NextPage = () => {
 
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name === "") {
       return setError("formErrors", {
         message: "Email OR Phone number are required. You need to choose one.",
       });
     }
-    editProfile({
-      email,
-      phone,
-      name,
-    });
+    if (avatar && avatar.length > 0) {
+      const cloudflareRequest = await (await fetch(`/api/files`)).json();
+      console.log(cloudflareRequest);
+      return;
+
+      editProfile({
+        email,
+        phone,
+        name,
+      });
+    } else {
+      editProfile({
+        email,
+        phone,
+        name,
+      });
+    }
   };
 
   useEffect(() => {
