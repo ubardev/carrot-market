9fcb2670b8880a1346de432c51af0dd64c447eb9
Ubar <bellin83@gmail.com>
2022-06-04 07:40:41 +0900

    :sparkles: 19.10 getStaticProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Script
         src="https://connect.facebook.net/en_US/sdk.js"
         onLoad={() => {
+          //@ts-ignore
           window.fbAsyncInit = function () {
+            //@ts-ignore
             FB.init({
               appId: "your-app-id",
               autoLogAppEvents: true,
